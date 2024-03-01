3b50d562a159cfe4dbaaac456e32138cb3efb312
markSancho7 <marcos.sancho7@alumnostrazos.net>
2024-03-01 12:08:21 +0100

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,66 @@
+import { useState } from 'react';
+import { USERS } from './constants/users';
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	const users = USERS;
+	const [firstValue, setFirstValue] = useState(0);
+	const [optionValue, setOptionValue] = useState(1);
+
+	const [pages, setPages] = useState(1);
+	console.log('numero de paginas', pages);
+	console.log(optionValue);
+
+	const pagedUsers = users.slice(firstValue, optionValue);
+
+	return (
+		<>
+			<select onClick={event => checkValue(event, setOptionValue)}>
+				<option value='1'>1</option>
+				<option value='2'>2</option>
+				<option value='3'>3</option>
+				<option value='4'>4</option>
+			</select>
+
+			<button onClick={prevPage}>prev</button>
+			<p>{pages}</p>
+			<button
+				onClick={() =>
+					nextPage(
+						optionValue,
+						users.length,
+						setPages,
+						setFirstValue,
+						setOptionValue
+					)
+				}
+			>
+				next
+			</button>
+
+			<h1>usuarios paginados</h1>
+			{pagedUsers.map(user => (
+				<div key={user.userId}>
+					<img src={user.profileImage} alt='' />
+					<p>{user.name}</p>
+				</div>
+			))}
+		</>
+	);
+};
+const checkValue = (event, setOptionValue) => {
+	setOptionValue(event.target.value);
+};
+const prevPage = () => {};
+const nextPage = (
+	optionValue,
+	usersLength,
+	setPages,
+	setFirstValue,
+	setOptionValue
+) => {
+	const numberOfPages = usersLength / optionValue;
+	const totalNumberOfPages = Math.ceil(numberOfPages);
+	setPages(totalNumberOfPages);
 };
 
 export default App;
---
+++ src/constants/users.js
@@ -0,0 +1,37 @@
+export const USERS = [
+	{
+		userId: 'b0b27b53-7c35-4c18-82fc-70190ca8ab67',
+		name: 'Josep Flores',
+		profileImage: 'https://randomuser.me/api/portraits/women/24.jpg'
+	},
+	{
+		userId: 'f0d07a91-e224-421c-9ade-274184545451',
+		name: 'Ricardo Rosas',
+		profileImage: 'https://randomuser.me/api/portraits/men/57.jpg'
+	},
+	{
+		userId: '1e4b5274-c11d-4bd8-a8bf-23aefd38555a',
+		name: 'Iván Tamayo',
+		profileImage: 'https://randomuser.me/api/portraits/men/9.jpg'
+	},
+	{
+		userId: 'a1277d44-7401-4946-a89e-5fbe2fb54344',
+		name: 'Maica Villanueva',
+		profileImage: 'https://randomuser.me/api/portraits/women/4.jpg'
+	},
+	{
+		userId: '4cd323a9-d1ae-4e64-8237-6cb797192c42',
+		name: 'Pedro Estrada',
+		profileImage: 'https://randomuser.me/api/portraits/men/2.jpg'
+	},
+	{
+		userId: '33e40d94-28c7-45b8-b367-8ff0ccb79c8d',
+		name: 'Jorge Cedillo',
+		profileImage: 'https://randomuser.me/api/portraits/men/88.jpg'
+	},
+	{
+		userId: '2aec7f93-e62b-4791-a921-b57f3df3eda3',
+		name: 'Ana Carreón',
+		profileImage: 'https://randomuser.me/api/portraits/women/59.jpg'
+	}
+];
